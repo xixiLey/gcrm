f25abc02672cf12f6b35122f9aa579cacaf18d3b
xixiLey <xixiley@sina.com>
2016-02-25 11:46:22 +0800

    github update 01
    github update 01

--- src/com/atguigu/test/Test01.java
+++ src/com/atguigu/test/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("eclipse hello world");
 		System.out.println("eclipse update 01");
+		System.out.println("github update 01");
 	}
 }
