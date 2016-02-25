8c32be30f34f5012a384cfddbd00bb5a237028eb
Lee <xixiley@sina.com>
2016-02-25 11:43:50 +0800

    eclipse 更新 Test01.java 01

--- src/com/atguigu/test/Test01.java
+++ src/com/atguigu/test/Test01.java
@@ -3,5 +3,6 @@ package com.atguigu.test;
 public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("eclipse hello world");
+		System.out.println("eclipse update 01");
 	}
 }
